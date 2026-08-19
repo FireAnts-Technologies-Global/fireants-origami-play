a115872b8f90f18699e0be2b523b67702597f377
anr <longtq2609@gmail.com>
2026-08-19 16:34:26 +0700

    Update dependencies injection for repositories in AppModule and add assets

--- app/src/main/java/com/fireants/template/di/AppModule.kt
+++ app/src/main/java/com/fireants/template/di/AppModule.kt
@@ -3,6 +3,18 @@ package com.fireants.template.di
 import android.content.Context
 import com.fireants.template.data.pref.AppSharedPref
 import com.fireants.template.data.pref.AppSharedPreferencesApp
+import com.fireants.template.data.repository.GameRepository
+import com.fireants.template.data.repository.KirigamiRepository
+import com.fireants.template.data.repository.Origami3DRepository
+import com.fireants.template.data.repository.OrigamiRepository
+import com.fireants.template.data.repository.UserRepository
+import com.fireants.template.data.repository.impl.GameRepositoryImpl
+import com.fireants.template.data.repository.impl.KirigamiRepositoryImpl
+import com.fireants.template.data.repository.impl.Origami3DRepositoryImpl
+import com.fireants.template.data.repository.impl.OrigamiRepositoryImpl
+import com.fireants.template.data.repository.impl.UserRepositoryImpl
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,9 +39,29 @@ class AppModule {
 
     @Singleton
     @Provides
-    fun provideMoshi(): com.squareup.moshi.Moshi {
-        return com.squareup.moshi.Moshi.Builder()
-            .addLast(com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory())
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .addLast(KotlinJsonAdapterFactory())
             .build()
     }
+
+    @Singleton
+    @Provides
+    fun provideGameRepository(impl: GameRepositoryImpl): GameRepository = impl
+
+    @Singleton
+    @Provides
+    fun provideKirigamiRepository(impl: KirigamiRepositoryImpl): KirigamiRepository = impl
+
+    @Singleton
+    @Provides
+    fun provideOrigami3DRepository(impl: Origami3DRepositoryImpl): Origami3DRepository = impl
+
+    @Singleton
+    @Provides
+    fun provideOrigamiRepository(impl: OrigamiRepositoryImpl): OrigamiRepository = impl
+
+    @Singleton
+    @Provides
+    fun provideUserRepository(impl: UserRepositoryImpl): UserRepository = impl
 }
