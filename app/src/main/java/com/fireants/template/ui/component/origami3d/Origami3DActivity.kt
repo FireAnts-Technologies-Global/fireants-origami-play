df5ea19404870ba2273d85f768a53d3b4fba979c
anr <longtq2609@gmail.com>
2026-08-20 11:38:54 +0700

    Refactor MainActivity header UI and add settings navigation

--- app/src/main/java/com/fireants/template/ui/component/origami3d/Origami3DActivity.kt
+++ app/src/main/java/com/fireants/template/ui/component/origami3d/Origami3DActivity.kt
@@ -1,9 +1,11 @@
 package com.fireants.template.ui.component.origami3d
 
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import com.fireants.template.R
 import com.fireants.template.databinding.ActivityOrigami3dBinding
 import com.fireants.template.ui.bases.BaseActivity
+import com.fireants.template.ui.bases.ext.click
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,5 +15,18 @@ class Origami3DActivity : BaseActivity<ActivityOrigami3dBinding>() {
 
     override fun getLayoutActivity(): Int {
         return R.layout.activity_origami_3d
+    }
+
+    override fun initViews() {
+        super.initViews()
+        mBinding.toolBar.tvTitle.text = getString(R.string.origami_3d)
+        mBinding.glowBackground.glowColor = ContextCompat.getColor(this, R.color.color_5BC2FB)
+    }
+
+    override fun onClickViews() {
+        super.onClickViews()
+        mBinding.toolBar.imgBack.click {
+            onBackPressed()
+        }
     }
 }
